51fb74b9e6bfff0edd2681732cdefbd03e426505
Alamsyah H. <45491807+tenggara25@users.noreply.github.com>
2026-03-22 23:51:04 +0700

    play video youtube

--- mobileprogramming/lib/pages/video_page.dart
+++ mobileprogramming/lib/pages/video_page.dart
@@ -1,15 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class VideoPage extends StatelessWidget{
+class VideoPage extends StatefulWidget {
   const VideoPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Video Page",
-        style: TextStyle(fontSize: 20),
-    ),
+  State<VideoPage> createState() => _VideoPageState();
+}
+
+class _VideoPageState extends State<VideoPage> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = YoutubePlayerController(
+      initialVideoId: 'dQw4w9WgXcQ', // Ganti dengan ID video YouTube yang diinginkan
+      flags: const YoutubePlayerFlags(
+        autoPlay: false,
+        mute: false,
+      ),
     );
-    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Video Youtube'),
+      ),
+      body: YoutubePlayer(
+        controller: _controller,
+        showVideoProgressIndicator: true,
+      ),
+    );
+  }
 }
